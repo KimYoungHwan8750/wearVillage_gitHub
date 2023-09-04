2126725aef0cc025478ab3be75237c79fbe882fa
KimYoungHwan8750 <dnfwlq123@naver.com>
2023-09-04 17:32:13 +0900

    upload

--- wearVillage/wearVillageServer/wearVillage/src/main/resources/static/js/header.js
+++ wearVillage/wearVillageServer/wearVillage/src/main/resources/static/js/header.js
@@ -6,7 +6,7 @@ const searchText_originalPlaceholder = searchText_focus.getAttribute('placeholde
 
 searchText_focus.addEventListener('focusin',()=>{
     searchArea_width.setAttribute('style','width:500px;');
-    searchText_focus.setAttribute('style','width:470px; text-align:left; font-size:18px')
+    searchText_focus.setAttribute('style','width:485px; text-align:left; font-size:18px')
     searchText_focus.setAttribute('placeholder','');
 })
 
@@ -22,7 +22,7 @@ searchText_focus.addEventListener('focusout',()=>{
 
     // .rectangle 요소에 클릭 이벤트 리스너를 추가합니다.
     rectangle.addEventListener('click', () => {
-        menu.style.right = menu.style.right === '0' ? '-250px' : '0';
+        menu.style.right = menu.style.left === '0' ? '-250px' : '0';
     });
 
     // 문서의 다른 영역을 클릭했을 때 메뉴를 닫는 이벤트 리스너를 추가합니다.
@@ -31,3 +31,22 @@ searchText_focus.addEventListener('focusout',()=>{
             menu.style.right = '-250px';
         }
     });
+
+
+const mobile_searchArea_width = document.querySelector('.header_mobileSearch');
+const mobile_searchText_focus = document.querySelector('.header_mobile_searchText');
+const mobile_searchArea_originalStyle = mobile_searchArea_width.getAttribute('style');
+const mobile_searchText_originalStyle = mobile_searchText_focus.getAttribute('style');
+const mobile_searchText_originalPlaceholder = mobile_searchText_focus.getAttribute('placeholder');
+
+mobile_searchText_focus.addEventListener('focusin',()=>{
+    mobile_searchArea_width.setAttribute('style','width:250px;');
+    mobile_searchText_focus.setAttribute('style','width:235px; text-align:left; font-size:18px')
+    mobile_searchText_focus.setAttribute('placeholder','');
+})
+
+mobile_searchText_focus.addEventListener('focusout',()=>{
+    mobile_searchArea_width.setAttribute('style',mobile_searchArea_originalStyle);
+    mobile_searchText_focus.setAttribute('style',mobile_searchText_originalStyle)
+    mobile_searchText_focus.setAttribute('placeholder',mobile_searchText_originalPlaceholder);
+})
