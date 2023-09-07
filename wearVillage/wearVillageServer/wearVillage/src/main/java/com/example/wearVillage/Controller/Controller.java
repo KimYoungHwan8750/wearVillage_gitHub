45020ec231b760c79c557a5af727bbec357ebf1d
toratsugumi37 <amematsu37373@gmail.com>
2023-09-07 11:44:29 +0900

    '초기버전'

--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/Controller/Controller.java
+++ wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/Controller/Controller.java
@@ -1,26 +1,52 @@
 package com.example.wearVillage.Controller;
+import static com.example.wearVillage.dataContoller.LoginCheck.*;
+import static com.example.wearVillage.dataContoller.check_email.*;
+import static com.example.wearVillage.dataContoller.check_id.*;
+import static com.example.wearVillage.dataContoller.createUserToOracle.*;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import javax.imageio.ImageIO;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-
-
-import static com.example.wearVillage.dataContoller.LoginCheck.login_check;
-import static com.example.wearVillage.dataContoller.check_email.check_email;
-import static com.example.wearVillage.dataContoller.check_id.check_id;
-import static com.example.wearVillage.dataContoller.createUserToOracle.dataToOracle;
-
-
+import com.example.wearVillage.AttachImageVO;
+import com.example.wearVillage.dataContoller.uploadTest;
 
 @org.springframework.stereotype.Controller
+@CrossOrigin(origins ={"http://localhost:8090/posting","http://localhost:8090/maps","http://localhost:8090/map_popup"})
 public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(uploadTest.class);
     // 메인화면으로 이동
     @RequestMapping(value = "/")
-    public String home(){
-            return "main.html";
+    public String home() {
+        return "main.html";
     }
+
+    
 
     //로그인 방식에 따라 회원가입 양식을 자동으로 채워주는 API
     @PostMapping(value = "/createUser")
@@ -93,4 +119,79 @@ public class Controller {
     public String makePopup() {
         return "maps.html";
     }
+
+    @PostMapping(value="/uploadTest", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    // public void uploadTestPOST(MultipartFile[] uploadFile) {
+    public ResponseEntity<List<AttachImageVO>> uploadTest(MultipartFile[] uploadFile) {
+        System.out.println("응답받아쪙");
+        logger.info("uploadTestPOST............");
+
+        for (int i = 0; i < uploadFile.length; i++) {
+            logger.info("------");
+            logger.info("파일 이름: " + uploadFile[i].getOriginalFilename());
+            logger.info("파일 타입: " + uploadFile[i].getContentType());
+            logger.info("파일 크기 :" + uploadFile[i].getSize());
+        }
+
+        // 내가 업로드 파일을 저장할 경로
+        String uploadFolder = "C:\\upload";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String str = sdf.format(date);
+        String datePath = str.replace("-", File.separator);
+        File uploadPath = new File(uploadFolder, datePath);
+        if (uploadPath.exists() == false) {
+            uploadPath.mkdirs();
+        }
+
+        List<AttachImageVO> list = new ArrayList();
+        for (MultipartFile multipartFile : uploadFile) {
+            AttachImageVO vo = new AttachImageVO();
+            String uploadFileName = multipartFile.getOriginalFilename();
+            vo.setFileName(uploadFileName);
+            vo.setUploadPath(uploadFolder);
+            // 저장할 파일, 생성자로 경로와 이름을 지정해줌.
+            String uuid = UUID.randomUUID().toString();
+            vo.setUuid(uuid);
+            uploadFileName = uuid + "_" + uploadFileName;
+            File saveFile = new File(uploadFolder, uploadFileName);
+            File thumbnailFile = new File(uploadPath, "s_" + uploadFileName);
+
+            try {
+                multipartFile.transferTo(saveFile);
+                BufferedImage bo_image = ImageIO.read(saveFile);
+                double ratio = 3;
+                int width = (int) (bo_image.getWidth() / ratio);
+                int height = (int) (bo_image.getHeight() / ratio);
+                BufferedImage bt_image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+                Graphics2D graphic = bt_image.createGraphics();
+                graphic.drawImage(bo_image, 0, 0, width, height, null);
+                ImageIO.write(bt_image, "jpg", thumbnailFile);
+                // void transferTo(File dest) throws IOException 업로드한 파일 데이터를 지정한 파일에 저장
+
+            } catch (Exception e) {
+                logger.error("Error occurred while file uploading", e);
+            }
+            list.add(vo);
+        }
+        ResponseEntity<List<AttachImageVO>> result = new ResponseEntity<List<AttachImageVO>>(list,
+                HttpStatus.OK);
+
+        return result;
+    }
+    
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getImage(String fileName) {
+        File file = new File("c:\\upload\\" + fileName);
+        ResponseEntity<byte[]> result = null;
+        try{
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
 }
