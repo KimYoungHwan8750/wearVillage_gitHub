c2a1b765910f9d01f823bf21e3d1f9f9f54df90a
toratsugumi37 <amematsu37373@gmail.com>
2023-09-20 14:34:26 +0900

    d
    d

--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/status/connect_info.java
+++
@@ -1,9 +0,0 @@
-package com.example.wearVillage.status;
-
-import static com.example.wearVillage.status.local_or_server.status;
-
-public class connect_info {
-    public static String user = status=="local"?"c##wearVillage":"admin";
-    public static String pw = status=="local"?"wearVillage":"admin12345";
-    public static  String url = status=="local"?"jdbc:oracle:thin:@localhost:1521:xe":"jdbc:oracle:thin:@wearvillage.c38c15agkmuv.ap-northeast-2.rds.amazonaws.com:1521:VILLAGE";
-}
--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/status/local_or_server.java
+++
@@ -1,6 +0,0 @@
-package com.example.wearVillage.status;
-
-public class local_or_server {
-    // public static String status = "server";
-    public static String status = "local";
-}
