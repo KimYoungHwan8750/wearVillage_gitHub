0001b9522546650207b36ff4503ac21427eabceb
developerKYH <dnfwlq123@naver.com>
2023-09-25 17:31:25 +0900

    Update test1.java

--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/testArea/test1.java
+++ wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/testArea/test1.java
@@ -5,12 +5,14 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 public class test1 {
-    public static final String test = "^[\\d]*$";
+    public String test(String id){
+    return id;
+    }
+
     public static void main(String[] args) {
-        Set<String> testSet = new HashSet<>();
-        testSet.add("321");
-        testSet.add("369+");
-        System.out.println(testSet.size());
+        test1 test = new test1();
+
+
 
     }
 }
