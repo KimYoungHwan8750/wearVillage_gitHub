30d7404f9c9496434c542f2073afffd679761b50
developerKYH <dnfwlq123@naver.com>
2023-09-27 17:46:40 +0900

    upload

--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/testArea/test1.java
+++ wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/testArea/test1.java
@@ -1,6 +1,7 @@
 package com.example.wearVillage.testArea;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -8,15 +9,7 @@ public class test1 {
 
 
     public static void main(String[] args) {
-        StringBuffer str = new StringBuffer();
-        str.append("12345");
-        System.out.println(str);
-        str.insert(3,"'3.5'");
-        System.out.println(str);
-        str.delete(2,5);
-        System.out.println(str);
-
-
+        Map<String,Object> test =;
 
 
     }
