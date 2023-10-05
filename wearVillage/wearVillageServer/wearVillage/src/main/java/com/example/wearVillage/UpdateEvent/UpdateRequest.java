f1e25062afb16d178c95eba74ab7c3dc964ce341
toratsugumi37 <amematsu37373@gmail.com>
2023-10-05 17:11:55 +0900

    20231005:17:11수정기능완성찐완성

--- wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/UpdateEvent/UpdateRequest.java
+++ wearVillage/wearVillageServer/wearVillage/src/main/java/com/example/wearVillage/UpdateEvent/UpdateRequest.java
@@ -1,5 +1,7 @@
 package com.example.wearVillage.UpdateEvent;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,13 +9,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UpdateRequest {
+    @NotBlank(message = "공백은 불가능합니다")
+    @Size(min=1, max=20,message = "한글자 이상, 20글자 미만으로 입력해주세요")
     private String postSubtitle;
+    @NotBlank(message = "공백은 불가능합니다")
     private String postPrice;
     private String postRentDefaultPrice;
     private String postRentDayPrice;
+    @NotBlank(message = "공백은 불가능합니다")
     private String postTagTop;
+    @NotBlank(message = "공백은 불가능합니다")
     private String postTagMiddle;
+    @NotBlank(message = "공백은 불가능합니다")
     private String postMapInfo;
+    @NotBlank(message = "공백은 불가능합니다")
     private String postText;
     private String postModifyDate;
 }
